fe772b6965e40be616a39453fba93fc03f0f436d
Wagner Cardoso <wagnerdev01@gmail.com>
2025-03-06 20:12:19 -0300

    fix(post layout): add table element responsive

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,3 +1,5 @@
+const { JSDOM } = require("jsdom");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
 
@@ -9,6 +11,23 @@ module.exports = function(eleventyConfig) {
       timeZone: 'UTC'
     })
   })
+
+  eleventyConfig.addTransform("wrapTables", function(content, outputPath) {
+    if (outputPath?.endsWith(".html")) {
+      const dom = new JSDOM(content);
+      const document = dom.window.document;
+
+      document.querySelectorAll("table").forEach(table => {
+        const wrapper = document.createElement("div");
+        wrapper.className = "table-container";
+        table.parentNode.insertBefore(wrapper, table);
+        wrapper.appendChild(table);
+      });
+
+      return dom.serialize();
+    }
+    return content;
+  });
 
   return {
     dir: {
